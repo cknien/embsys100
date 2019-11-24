ca279be60183a1826badfc853b36819eae3d0399
cknien <56375436+cknien@users.noreply.github.com>
2019-11-24 14:47:16 -0800

    Add files via upload

---
+++ assignment05/Problem 3/src/main.c
@@ -0,0 +1,87 @@
+/*******************************************************************************
+FILE NAME   : main.c
+DESCRIPTION : Where all the magic begins.
+	      
+
+Copyright   : David Allegre
+            : 2015
+	    : All Rights Reserved
+*******************************************************************************/
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "nucleoboard.h"
+#include "hw_init.h"
+#include "print.h"
+
+
+char clr_scrn[] = { 27, 91, 50, 74, 0 };              // esc[2J
+/* Public variables ----------------------------------------------------------*/
+__IO uint32_t timer;
+PRINT_DEFINEBUFFER();           /* required for lightweight sprintf */
+/* Private prototype ---------------------------------------------------------*/
+void delay(uint32_t time);
+
+uint32_t sqrAsm(int val);
+uint32_t div2Asm(int val);
+const char* myCstr = "\nHello from assembly! Let's do square calculation!\n ";
+const char* myCstr_div = "\nHello from assembly! Let's do dividing by 2 calculation!\n ";
+
+/*******************************************************************************
+Function Name   : main
+Description     : 
+Parameters      :
+Return value    :               */
+void main() {
+uint8_t foo;
+
+  foo = 10;
+  Hw_init();
+
+  PrintString(clr_scrn); /* Clear entire screen */
+  // Change [My name] to your name      //
+  PrintString("Chun-Wei Nien's Nucleo STM32F401 is ... alive!!!\n");
+  RETAILMSG(1, ("Programming Assignment05 #2: Built %s %s.\r\n\r\n",
+              __DATE__,
+              __TIME__));  
+
+  while (foo) {
+    GPIO_ToggleBits(GPIO_PORT_LD2, GPIO_PIN_LD2);
+    //PrintString("EMBSYS CP100A: University of Washington - UART Test Application \n");
+    
+    delay(1000);
+    GPIO_ToggleBits(GPIO_PORT_LD2, GPIO_PIN_LD2);
+    
+    delay(1000);
+    
+    PrintString("x = ");
+    Print_uint32(foo);
+    PrintString("\n");
+    
+    //uint32_t y = sqrAsm(foo);
+    uint32_t y = div2Asm(foo);
+    PrintString("\n");
+    
+    PrintString("x/2 = ");
+    Print_uint32(y);
+    PrintString("\n\n");
+    
+    foo--;
+  }
+  while (1) {
+    // loop forever
+    asm("nop");
+  }
+}
+
+
+void delay(uint32_t time) {
+  
+  timer = time;
+  while(timer) {}
+}
+void SystemInit(void) {
+  // System init is called from the assembly .s file
+  // We will configure the clocks in hw_init
+  asm("nop");
+}
+  
